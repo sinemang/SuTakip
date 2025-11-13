7ad9748d36db292ab9efe8afe7beed8f35403f7f
Sinem Angı <angisinem99@gmail.com>
2025-11-13 16:01:17 +0300

    Yaptığım değişiklikleri kaydediyorum

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,22 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:water_reminder/firebase_options.dart';
 import 'app/routes.dart';
 import 'app/theme.dart';
+import 'core/firebase/auth_service.dart';
+import 'firebase_options.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options:DefaultFirebaseOptions.currentPlatform,);
+
+  final authService =AuthService();//auth servis nesnesi oluşturuldu,firebase auth erişimi hazır
+  await authService.signInAnonymouslyIfNeeded();//çağırıldı ve otomatik anonim oturum açıldı
+
+  print("current user UID: ${authService.uid}");
+
+
   runApp(const WaterReminderApp());
 }
 
